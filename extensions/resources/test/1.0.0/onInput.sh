dc111a2716eab77b83524e6f241aff4c49a4dbcc
jfrogtw <giannit@jfrog.com>
2020-08-05 08:35:59 -0700

    Revert "Add extensions"
    This reverts commit 9f968bfbdb94a85200ea559970ddb995f9d77856.

--- extensions/resources/test/1.0.0/onInput.sh
+++
@@ -1,6 +0,0 @@
-test_input() {
-  echo "resource name: $1"
-  printenv
-}
-
-execute_command "test_input %%context.resourceName%%"
--- extensions/resources/test/1.0.0/onOutput.sh
+++
@@ -1,6 +0,0 @@
-test_output() {
-  echo "resource name: $1"
-  printenv
-}
-
-execute_command "test_output %%context.resourceName%%"
